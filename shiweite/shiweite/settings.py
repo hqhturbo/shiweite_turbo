0091a418ef7b7c12131375caede4abd014ef7b3f
TeFuir02 <TeFuir02@users.noreply.github.com>
2021-09-24 18:08:35 +0800

    项目配置redis

--- shiweite/shiweite/settings.py
+++ shiweite/shiweite/settings.py
@@ -125,3 +125,27 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+# 配置缓存
+CACHES = {
+    "default": {  # 默认
+        "BACKEND": "django_redis.cache.RedisCache",  # 设置为redis缓存
+        "LOCATION": "redis://127.0.0.1:6379/0",  # 指定redis地址。0表示为0号库
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": "123456",
+        }
+    },
+    "session": {  # session
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": "redis://127.0.0.1:6379/1",
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+            "PASSWORD": "123456",
+        }
+    },
+}
+# session由数据库存储改为redis
+SESSION_ENGINE = "django.contrib.sessions.backends.cache"
+SESSION_CACHE_ALIAS = "session"
