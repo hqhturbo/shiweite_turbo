61dc92006a1a9e98e59d4bb219c0a0ec578b390b
TeFuir02 <TeFuir02@users.noreply.github.com>
2021-09-26 17:30:22 +0800

    定义用户模型类并迁移

--- shiweite/shiweite/settings.py
+++ shiweite/shiweite/settings.py
@@ -197,3 +197,6 @@ LOGGING = {
         },
     }
 }
+
+#指定本项目用户模型类
+AUTH_USER_MODEL = 'users.User'
--- shiweite/users/models.py
+++ shiweite/users/models.py
@@ -1,3 +1,27 @@
 from django.db import models
+# 导入系统抽象类用户
+from django.contrib.auth.models import AbstractUser
+
 
 # Create your models here.
+# 自定义用户类
+class User(AbstractUser):
+    # 手机号
+    # max_length:手机号长度11位；unique：手机号是唯一的；blank:手机号为必填项
+    mobile = models.CharField(max_length=11, unique=True, blank=False)
+
+    # 头像信息
+    # upload_to:上传文件地址为：avatar文件夹下以年月日方式命名文件；blank:头像不是必填项
+    avatar = models.ImageField(upload_to='avatar/%Y%m%d/', blank=True)
+
+    # 简介信息
+    # max_length:简介长度500；blank：用户简介不是必填项
+    user_desc = models.CharField(max_length=500, blank=True)
+
+    class Meta:
+        db_table = 'tb_users'  # 修改表名
+        verbose_name = '用户管理'  # admin 后台显示
+        verbose_name_plural = verbose_name  # admin 后台显示
+
+    def __str__(self):
+        return self.mobile
