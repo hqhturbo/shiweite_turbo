9eefccfb9553fd1b4e97988f2202430e72d58a98
TeFuir02 <TeFuir02@users.noreply.github.com>
2021-09-27 16:18:39 +0800

    短信验证码实现

--- shiweite/users/urls.py
+++ shiweite/users/urls.py
@@ -1,9 +1,14 @@
 from django.contrib import admin
-from users.views import RegisterView,ImageView
+from users.views import RegisterView,ImageView,SmsCodeView
 from django.urls import path, include
 
 urlpatterns = [
+    # 参数1：路由
+    # 参数2：视图函数
+    # 参数3：路由名，方便通过reverse来获取路由
     path('register/', RegisterView.as_view(), name='register'),
-    path('imagecode/',ImageView.as_view(),name='imagecode')
+    # 图片验证码路由
+    path('imagecode/',ImageView.as_view(),name='imagecode'),
+    path('smscode/', SmsCodeView.as_view(),name='smcode')
 ]
 
--- shiweite/users/views.py
+++ shiweite/users/views.py
@@ -1,10 +1,18 @@
+from random import randint
+
+from django.http import HttpResponseBadRequest, JsonResponse
 from django.shortcuts import render,HttpResponse
 from django.views import View
 from libs.captcha.captcha import captcha  # 导入图片验证码库
 from django_redis import get_redis_connection  # 导入redis包
+from libs.yuntongxun.CCPRestSDK import *
+from libs.yuntongxun.sms import CCP
 # Create your views here.
 
 # 注册
+from utils.response_code import RETCODE
+
+
 class RegisterView(View):
     def get(self,request):
         return render(request,'register.html')
@@ -35,3 +43,49 @@ class ImageView(View):
         redis_conn.setex(name='img:%s' % uuid, time=300, value=text)
         # 5、返回图片给前端
         return HttpResponse(image, content_type='image/jpeg')
+
+# 短信验证码
+class SmsCodeView(View):
+    def get(self,request):
+        '''
+            实现思路：
+            1、接收参数
+            2、参数验证
+                2.1 验证参数是否齐全
+                2.2 图片验证码的验证
+                    连接reids，获取reids中图片验证码
+                    判断图片验证码是否存在
+                        如果图片验证码未过期，我们获取到之后就可以删除图片验证码
+                        比对图片验证码（忽略用户大小写）
+            3、生成短信验证码
+            4、保存短信验证码内容到redis中
+            5、发送短信
+            6、返回响应信息
+            :param request:
+            :return:
+        '''
+        mobile = request.GET.get('mobile')
+        image_code = request.GET.get('image_code')
+        uuid = request.GET.get('uuid')
+        if not all([mobile,image_code,uuid]):
+            return JsonResponse({'code':RETCODE.NECESSARYPARAMERR,'errmsg':'参数不全'})
+        # 图片验证码的验证
+        redis_conn = get_redis_connection()
+        # 从redis中获取已经存入的图片验证码信息
+        image_code_redis = redis_conn.get('img:%s' % uuid)
+        if image_code_redis is None:
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图片验证码不存在'})
+            # return HttpResponseBadRequest(content='图片验证码不存在')
+        # 删除redis中已经验证的图片验证码
+        redis_conn.delete('img:%s' % uuid)
+        # redis中取出的数据需要解码
+        if image_code.lower() !=image_code_redis.decode().lower():
+            return JsonResponse({'code': RETCODE.IMAGECODEERR, 'errmsg': '图片验证码错误'})
+            # return HttpResponseBadRequest(content='图片验证码错误')
+
+        sms_code = '%06d' % randint(0,999999)
+        print(f'你的验证码是：{sms_code}')
+        ccp = CCP()
+        ccp.send_template_sms('17691149837', [sms_code, 5], 1)
+        return JsonResponse({'code': RETCODE.OK, 'errmsg': '验证码发送成功'})
+
